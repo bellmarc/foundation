c230eb3aecdc254597b00818fca90c85aee3df6e
Marcielo Bell <marcielobell@gmail.com>
2019-09-01 22:05:45 -0600

    Working with CSS grid for business project

---
+++ projects/business-time/index.js
@@ -0,0 +1,9 @@
+// function initMap(){
+//     let location = {
+//         lat: 49.487457, lng: 8.466040
+//     };
+//     let map = new google.maps.Map(document.getElementById("map"),{
+//      zoom: 4,
+//      center: location
+//     });
+// }
