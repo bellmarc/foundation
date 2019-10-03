1451a49fa0c9b25dda619ce823b0b03d576a6736
Marcielo Bell <marcielobell@gmail.com>
2019-10-02 20:51:44 -0600

    Card interpolation transform effect added

--- projects/asoiaf-api/src/components/HouseCard.js
+++ projects/asoiaf-api/src/components/HouseCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {withChar} from '../context/CharacterProvider.js';
+import { useSpring, animated } from 'react-spring'
 
-//rcvs props from HouseList
+
+// animation on card
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
 const HouseCard = (props) => {
+    const [move, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
     // console.log(props.houses.swornMembers)
     return (
         <>
@@ -14,13 +20,13 @@ const HouseCard = (props) => {
                     pathname: "/members",
                     state: {swornMembers: props.swornMembers}
             }}>
-                <span className="card" style={{'display': 'block'}}>
-                     <h2 className="house-name">{props.name}
-
+                <animated.div className="card" onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })} onMouseLeave={() => set({ xys: [0, 0, 1] })} style={{'display': 'block', transform: move.xys.interpolate(trans) }}
+                >
+                    <h2 className="house-name">{props.name}
                     </h2>
                     <h3 className="house-name">{props.region}</h3>
-                </span>
-                </Link>
+                </animated.div>
+            </Link>
 
         </div>
             {/* <img alt={house} style={imgStyle} src={imgURL}/> */}
--- projects/asoiaf-api/src/components/Navbar.js
+++ projects/asoiaf-api/src/components/Navbar.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import {Link, Switch, Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {withChar} from '../context/CharacterProvider.js';
 
 
 const Navbar = (props) => {
     // console.log("Navbar", props.history.location)
-    // console.log(`/houses/${props.pageNum}`)
-    console.log(props)
-
-//     let currentTab
-//     if (props.history.location.pathname === `/houses/`){
-//         currentTab = "currentTab"
-//    } else {
-//         currentTab = "nav-highlight"
-//    }
-
 
     return (
         <nav className="currentTab">
-           <Link className={props.pathname==='/houses/' &&"nav-highlight"} to ="/houses/">Houses</Link>
-           <Link className={props.pathname==='/members' &&"nav-highlight"} to="/members"> Sworn Members</Link>
+           <Link className={props.pathname==='/houses/' && "nav-highlight"} to ="/houses/">Houses</Link>
+          {/* <Link className={props.pathname==='/members' &&"nav-highlight"} to="/members"> Sworn Members</Link> */}
            <Link className={props.pathname==='/characters/' &&"nav-highlight"} to="/characters">Characters</Link>
-           {/* <Link to="/map">Map</Link> */}
+           <Link className={props.pathname==='/map/' && "nav-highlight"} to="/map/">Map</Link>
         </nav>
     )
 }
