b83115d87c05d359a11a8890d23269010bd1ab27
Marcielo Bell <marcielobell@gmail.com>
2019-11-29 22:03:21 -0700

    working on hover featureand toggler

---
+++ projects/portfolio/client/src/component/CardEffect.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import '../style/CardEffect.css';
+
+const CardEffect = () => {
+    // const [toggle, setToggle] = useState(false);
+    const [hoverInfo, isHovered] = useHover();
+
+    const cardInfoToggler = ()=> {
+        // setToggle(prevToggle => !prevToggle)
+        // onmouseenter add info block and social media
+    }
+
+    return (
+        <div ref={hoverInfo} >
+            {isHovered ? 'true' : 'false'}
+            <h1>{ cardInfoToggler }</h1>
+        </div>
+    )
+}
+
+
+
+export default CardEffect
