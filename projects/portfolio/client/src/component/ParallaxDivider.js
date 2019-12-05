2a8c116a5555cbfa6149a3b9b5081f9c9da14ef8
Marcielo Bell <marcielobell@gmail.com>
2019-12-04 23:18:40 -0700

    added parallax images to projects

--- projects/portfolio/client/src/component/ParallaxDivider.js
+++ projects/portfolio/client/src/component/ParallaxDivider.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import '../style/ParallaxDivider.css';
-
+import gotMacbook from '../assets/got-macbook-view.png';
 
 const ParallaxDivider = () => {
     return (
-        <main class="wrapper">
-            <section class="section parallax bg1">
-                <h1>Vidly C2C Travel Web Application</h1>
+        <main className="wrapper">
+            <section className="section parallax bg1">
+                <h1>Game Of Thrones Fanpage </h1>
+                <a href="https://gameofthrones.herokuapp.com/">
+                <img src={ gotMacbook } className="macbook-img" alt="macbook view of GOT themed app"
+                />
+                </a>
             </section>
-            <section class="section static">
-                <h1> Technologies Used: </h1>
-                <h2>ReactJS, Express, Postman, Node.js, MongoDB</h2>
+            <section className="section static">
+                <h1> Technologies: </h1>
+                <h2>ReactJS, Express, Postman, Node, MongoDB</h2>
             </section>
-            <section class="section parallax bg2">
+            <section className="section parallax bg2">
                 <h1>Featured Destinations in Vidly Web App</h1>
             </section>
-            <section class="section static-link">
+            <section className="section static-link">
                 <h1> View Vidly Live  </h1>
+            </section>
+            <section className="section parallax bg3">
+                <h1>Vidly C2C Travel Web Application</h1>
+                <div>
+                {/* <img src={ gotMacbook } className="macbook-img" alt="macbook view of GOT themed app" /> */}
+                </div>
             </section>
         </main>
     )
---
+++ projects/portfolio/client/src/component/ReactRotatingText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../style//ReactRotatingText.css';
+
+
+const ReactRotatingText = () => {
+
+}
+
+export default ReactRotatingText
